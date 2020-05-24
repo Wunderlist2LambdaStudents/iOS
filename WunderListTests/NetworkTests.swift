11bce313a6ad143100db97f4f6f3333075fd2b06
Kenny Dubroff <kenny.dubroff@kennydubroff.com>
2020-05-24 10:26:54 -0700

    Added test for getting data from generic source

--- WunderListTests/NetworkTests.swift
+++ WunderListTests/NetworkTests.swift
@@ -11,24 +11,19 @@ import XCTest
 
 class NetworkTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testGettingData() {
+        let expectation = self.expectation(description: "Wait for google")
+        //test creating request
+        let request = NetworkService.createRequest(url: URL(string: "https://www.google.com"), method: .get)
+        XCTAssertNotNil(request)
+        //test loading data from request
+        NetworkService.loadData(using: request!) { (data, response, error) in
+            XCTAssertNotNil(data)
+            XCTAssertNotNil(response)
+            XCTAssertEqual(response?.statusCode, 200)
+            expectation.fulfill()
         }
+        wait(for: [expectation], timeout: 2.0)
     }
 
 }
