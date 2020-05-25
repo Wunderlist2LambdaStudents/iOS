a2be6e1726e1ba57e0a2f28b93812767630f5f3e
Kenny Dubroff <kenny.dubroff@kennydubroff.com>
2020-05-24 18:52:12 -0700

    Added Mock JSON Data for user and Todo

--- WunderListTests/NetworkTests.swift
+++ WunderListTests/NetworkTests.swift
@@ -19,6 +19,7 @@ class NetworkTests: XCTestCase {
         XCTAssertNotNil(request)
         //test loading data from request
         networkService.dataLoader.loadData(using: request!) { (data, response, error) in
+            XCTAssertNotNil(error)
             XCTAssertNotNil(data)
             XCTAssertNotNil(response)
             XCTAssertEqual(response?.statusCode, 200)
@@ -29,22 +30,43 @@ class NetworkTests: XCTestCase {
 
     func testDecodingMockUserData() {
         // FIXME: Once refactored to live user, remove this
-        struct MockUser: Decodable {
+        struct MockUser: Codable {
             let username: String
         }
         let expectation = self.expectation(description: "\(#file), \(#function): WaitForDecodingMockData")
         let mockDataLoader = MockDataLoader(data: Data.mockData(with: "GoodUserData"), response: nil, error: nil)
         let networkService = NetworkService(dataLoader: mockDataLoader)
-        let request = URLRequest(url: URL(string: "https://google.com")!)
+        var request = URLRequest(url: URL(string: "https://google.com")!)
         networkService.dataLoader.loadData(using: request) { (data, response, error) in
             XCTAssertNotNil(data)
             XCTAssertNil(response)
             XCTAssertNil(error)
             // FIXME: Once refactored to live user, change type ALSO CHANGE MOCK JSON TO MATCH BACKEND SAMPLE JSON
             let mockUser = networkService.decode(to: MockUser.self, data: data!)
+            networkService.encode(from: mockUser, request: &request)
+            networkService.dataLoader.loadData(using: foo.request) { (data, response, error) in
+
+            }
             XCTAssertNotNil(mockUser)
             expectation.fulfill()
         }
         wait(for: [expectation], timeout: 1.0)
+    }
+
+    func foo() {
+        let networkHelper = NetworkService()
+        let request = networkHelper.createRequest(url: URL(string: "https://google.com"), method: .get)
+        networkHelper.dataLoader.loadData(using: request!) { (data, response, error) in
+            if let error = error {
+                print("error getting google")
+                print(error)
+            }
+            if let response = response {
+                print(response.statusCode)
+            }
+            if let data = data {
+                print(data)
+            }
+        }
     }
 }
