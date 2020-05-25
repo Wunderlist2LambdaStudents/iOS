b744efeadbeb30c456dc03bba4de9cd653f4fe32
Mark Poggi <markp@mac.com>
2020-05-25 14:30:41 -0700

    Added enum to simplify accessing accessibility identifiers in LoginViewController.

--- WunderListUITests/WunderListUITests.swift
+++ WunderListUITests/WunderListUITests.swift
@@ -17,12 +17,20 @@ class WunderListUITests: XCTestCase {
                app.launch()
            }
 
+    enum Identifier: String {
+        case loginNameTextField = "LoginViewController.NameTextField"
+        case loginPasswordField = "LoginViewController.PWTextField"
+        case loginStartButton = "LoginViewController.GetStartedButton"
+        case loginTCLabel = "LoginViewController.T&CLabel"
+    }
+
     func testExample() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
 
         // Use recording to get started writing UI tests.
+
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
