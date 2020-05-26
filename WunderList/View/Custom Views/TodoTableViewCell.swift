f11219a60c65aebc4949b069c3a265d05ad7d126
Kenny Dubroff <kenny.dubroff@kennydubroff.com>
2020-05-26 16:39:05 -0700

    Linting

--- WunderList/View/Custom Views/TodoTableViewCell.swift
+++ WunderList/View/Custom Views/TodoTableViewCell.swift
@@ -12,7 +12,7 @@ class TodoTableViewCell: UITableViewCell {
 
     @IBOutlet weak var todoTitleLabel: UILabel!
     @IBOutlet weak var completeButton: UIButton!
-    
+
     var todo: Todo?
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -21,11 +21,10 @@ class TodoTableViewCell: UITableViewCell {
         func customInit(title: String, completeButton: UIButton) {
             self.todoTitleLabel.text = title
             self.completeButton = completeButton
-            
+
             self.todoTitleLabel.textColor = .black
             self.contentView.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         }
     }
-    
-}
 
+}
